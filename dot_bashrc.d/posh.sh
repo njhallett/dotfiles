63e3140f440e8992a868a576dcee1fd4c134568b
Niall Hallett <njhallett@gmail.com>
2023-11-18 15:53:51 +0000

    posh update ftn

--- dot_bashrc.d/posh.sh
+++ dot_bashrc.d/posh.sh
@@ -1,1 +1,12 @@
-eval "$(posh init bash --config ~/.local/share/poshthemes/rudolfs-dark.omp.json)"
+update_posh_themes() {
+    cache_dir=$(posh cache path)
+    theme_dir="${cache_dir}/themes"
+
+    mkdir -p "$theme_dir"
+    curl -s -L https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o "${cache_dir}/themes.zip"
+    unzip -o -q "${cache_dir}/themes.zip" -d "$theme_dir"
+    chmod u+rw "${theme_dir}/*.omp.*"
+    rm "${cache_dir}/themes.zip"
+}
+
+eval "$(posh init bash --config "$(posh cache path)/themes/gruvbox-basic-dark.omp.json")"
