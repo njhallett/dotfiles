3ca3f69cbe77013cddfaab0b31215b2f14af5a49
Niall Hallett <njhallett@gmail.com>
2024-09-07 15:20:14 +0100

    nnn setup

---
+++ dot_bashrc.d/nnn.sh
@@ -0,0 +1,11 @@
+export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview;z:autojump'
+export NNN_FIFO=/tmp/nnn.fifo
+
+BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+
+if [ -x ~/.local/bin/nnn-nerd-static ]; then
+    alias n=nnn-nerd-static
+else
+    alias n=nnn
+fi
