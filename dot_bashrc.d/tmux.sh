fbbc45077b41d83d5da14c81e0c89558c0410835
Niall Hallett <njhallett@gmail.com>
2025-07-15 15:56:30 +0100

    tmux function main

---
+++ dot_bashrc.d/tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function tmux_main {
+
+    if ! tmux has-session -t main 2>/dev/null; then
+        tmux new-session -s main -d
+        tmux send-keys -t main 'nnn' C-m
+        tmux split-window -h -t main
+    fi
+
+    tmux attach -t main
+}
