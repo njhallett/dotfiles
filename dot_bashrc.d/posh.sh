e5ddefe9d44741f86091f67eeb77ef98635b66bb
Niall Hallett <njhallett@gmail.com>
2025-12-01 14:30:28 +0000

    fix chmod and prevent running on tty

--- dot_bashrc.d/posh.sh
+++ dot_bashrc.d/posh.sh
@@ -1,3 +1,7 @@
+if ! command -v posh >/dev/null 2>&1 && command -v oh-my-posh >/dev/null 2>&1; then
+    alias posh=oh-my-posh
+fi
+
 update_posh_themes() {
     cache_dir=$(posh cache path)
     theme_dir="${cache_dir}/themes"
@@ -5,8 +9,8 @@ update_posh_themes() {
     mkdir -p "$theme_dir"
     curl -s -L https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o "${cache_dir}/themes.zip"
     unzip -o -q "${cache_dir}/themes.zip" -d "$theme_dir"
-    chmod u+rw "${theme_dir}/*.omp.*"
+    chmod u+rw "${theme_dir}/"*".omp."*
     rm "${cache_dir}/themes.zip"
 }
 
-eval "$(posh init bash --config "$(posh cache path)/themes/gruvbox-basic-dark.omp.json")"
+[[ $(tty) != "/dev/tty"* ]] && eval "$(posh init bash --config "$(posh cache path)/themes/gruvbox-basic-dark.omp.json")"
