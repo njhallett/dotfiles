5092cb230416d8c122adcb14e0c8e8e66766eca3
Niall Hallett <njhallett@gmail.com>
2025-12-14 14:05:34 +0000

    use nnn alias

--- dot_bashrc.d/nnn.sh
+++ dot_bashrc.d/nnn.sh
@@ -1,11 +1,12 @@
 export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview;z:autojump'
 export NNN_FIFO=/tmp/nnn.fifo
+export NNN_PISTOL=1
 
 BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
 export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
 
-if [ ! -e ~/.local/bin/nnn ] && [ -x ~/.local/bin/nnn-nerd-static ]; then
-    ln -s ~/.local/bin/nnn-nerd-static ~/.local/bin/nnn
+if [ -x ~/.local/bin/nnn ]; then
+    alias n=nnn
+elif [ -x ~/.local/bin/nnn-nerd-static ]; then
+    alias n=nnn-nerd-static
 fi
-
-alias n=nnn
--- dot_bashrc.d/tmux.sh
+++ dot_bashrc.d/tmux.sh
@@ -4,7 +4,7 @@ function tmux_main {
 
     if ! tmux has-session -t main 2>/dev/null; then
         tmux new-session -s main -d
-        tmux send-keys -t main 'nnn' C-m
+        tmux send-keys -t main 'n' C-m
         tmux split-window -h -t main
     fi
 
