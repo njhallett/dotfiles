9b6c5ffb664b6a955c730226a0538b732c1ac014
Niall Hallett <njhallett@gmail.com>
2026-06-12 08:45:49 +0100

    nnn, bat, pistol integration

--- dot_bashrc.d/nnn.sh
+++ dot_bashrc.d/nnn.sh
@@ -1,12 +1,23 @@
 export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview;z:autojump'
 export NNN_FIFO=/tmp/nnn.fifo
-export NNN_PISTOL=1
-
-BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
-export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+export NNN_FCOLORS="1F8C1202005F2309c66A5B6C"
 
 if [ -x ~/.local/bin/nnn-nerd-static ] && [ ! -x ~/.local/bin/nnn ]; then
     ln -s ~/.local/bin/nnn-nerd-static ~/.local/bin/nnn
+fi
+
+if grep -qEi "(Microsoft|Microsoft-Standard-WSL2)" /proc/version 2>/dev/null \
+   || [ -n "$SSH_CLIENT" ] \
+   || [ -n "$SSH_TTY" ] \
+   || [ -z "$DISPLAY" ]; then
+
+    if command -v pistol >/dev/null 2>&1; then
+        export NNN_OPENER="pistol"
+    else
+        export NNN_OPENER="$HOME/.config/nnn/plugins/nuke"
+    fi
+
+    export NNN_OPTS=c
 fi
 
 alias n=nnn
